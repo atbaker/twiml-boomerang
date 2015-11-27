16f7dfbe9fb5b19ef86d7f73dd05a3765c639681
Andrew T. Baker <abaker@twilio.com>
2015-11-27 16:50:38 -0500

    First stab

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+from twilio import twiml
+
+
+app = Flask(__name__)
+
+@app.route('/retry', methods=['GET', 'POST'])
+def retry_request():
+    """ bar """
+    resp = twiml.Response()
+
+    if 'ErrorCode' in request.values and request.values['ErrorCode'] == '11200' and False:
+        # The original request timed out, but let's try it again
+        resp.pause(length=10)
+        resp.redirect(request.values['ErrorUrl'],
+                      method=request.values.get('Method') or 'POST')
+    else:
+        # There's no hope for this request. Redirect to the provided fallback
+        # URL, or intentionally redirect to an error URL to invoke the official
+        # Twilio error message
+        # resp.redirect('http://fallback.antivoicemail.com/voice-error.xml', method='GET')
+        # resp.say('Sorry, an application error has occurred. Goodbye.', voice='alice')
+        if 'FallbackUrl' in request.values:
+            resp.redirect(request.values['FallbackUrl'],
+                          method=request.values.get('FallbackMethod') or 'GET')
+        else:
+            resp.redirect('/error')
+    
+    return str(resp)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
